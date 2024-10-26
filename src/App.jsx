59c978dbc7f94186c13bb437f9b571a2014f5528
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2024-10-26 12:45:58 -0700

    Created logo

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+<>
+<div id="main">
+<div id="main-inner">
+  <div id="title">
+    <h1 className="title" style={{lineHeight: "60px"}}>ENHANCE</h1>
+    <div className="title-bottom" style={{lineHeight: "15px"}}>
+      <div className="tb-left">openings</div>
+      <div className="tbr-center"> | </div>
+      <div className="tb-right">
+        <div className="tbr-top">WINDOWS,</div>
+        <div className="tbr-bottom">DOORS & MORE</div>
+      </div>
+    </div>
+  </div>
+
+</div>
+</div>
+</>
+  );
+}
+
+export default App;
